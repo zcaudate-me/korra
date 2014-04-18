61b2c4f9558c5243d1ba6d339688e88bbb6104fe
Chris Zheng <chris.zheng@auspost.com.au>
2014-04-18 19:25:14 +1000

    bumped version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject im.chit/korra "0.1.0"
+(defproject im.chit/korra "0.1.1"
   :description "jar and maven package introspection"
   :url "http://www.github.com/zcaudate/korra"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.cemerick/pomegranate "0.3.0"]
                  [version-clj "0.1.0"]
-                 [im.chit/iroh "0.1.6"]]
+                 [im.chit/iroh "0.1.10"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
                     :plugins [[lein-midje "3.1.3"]]}}
   :documentation {:files {"docs/index"
